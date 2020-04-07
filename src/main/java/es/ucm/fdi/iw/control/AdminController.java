f1c3052b84690e1338083a704706792cefda6f62
isaugar <62020260+isaugar@users.noreply.github.com>
2020-04-07 02:52:57 +0200

    user remove

--- src/main/java/es/ucm/fdi/iw/control/AdminController.java
+++ src/main/java/es/ucm/fdi/iw/control/AdminController.java
@@ -12,6 +12,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,50 +22,58 @@ import es.ucm.fdi.iw.model.User;
 
 /**
  * Admin-only controller
+ * 
  * @author mfreire
  */
 @Controller()
 @RequestMapping("admin")
 public class AdminController {
-	
+
 	private static final Logger log = LogManager.getLogger(AdminController.class);
-	
-	@Autowired 
+
+	@Autowired
 	private EntityManager entityManager;
-	
+
 	@Autowired
 	private LocalData localData;
-	
+
 	@Autowired
 	private Environment env;
-	
+
 	@GetMapping("/")
 	public String index(Model model) {
 		model.addAttribute("activeProfiles", env.getActiveProfiles());
 		model.addAttribute("basePath", env.getProperty("es.ucm.fdi.base-path"));
 
-		model.addAttribute("users", entityManager.createQuery(
-				"SELECT u FROM User u").getResultList());
-		
+		model.addAttribute("users", entityManager.createQuery("SELECT u FROM User u").getResultList());
+
 		return "admin";
 	}
-	
+
 	@PostMapping("/toggleuser")
 	@Transactional
-	public String delUser(Model model,	@RequestParam long id) {
+	public String delUser(Model model, @RequestParam long id) {
 		User target = entityManager.find(User.class, id);
 		if (target.getEnabled() == 1) {
 			// disable
-			File f = localData.getFile("user", ""+id);
+			File f = localData.getFile("user", "" + id);
 			if (f.exists()) {
 				f.delete();
 			}
 			// disable user
-			target.setEnabled((byte)0); 
+			target.setEnabled((byte) 0);
 		} else {
 			// enable user
-			target.setEnabled((byte)1);
+			target.setEnabled((byte) 1);
 		}
 		return index(model);
-	}	
+	}
+
+	@PostMapping("/delete")
+	@Transactional
+	public String delete(Model model, @RequestParam long id) {
+		User target = entityManager.find(User.class, id);
+		entityManager.remove(target);
+		return "redirect:/usuarios/";
+	}
 }
