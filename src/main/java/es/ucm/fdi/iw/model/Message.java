1f39365b26f1f44f6d4d5c7f37dd327896ef8dcf
sesanc09 <62522929+sesanc09@users.noreply.github.com>
2020-04-28 14:38:57 +0200

    sockets

--- src/main/java/es/ucm/fdi/iw/model/Message.java
+++ src/main/java/es/ucm/fdi/iw/model/Message.java
@@ -1,15 +1,22 @@
 package es.ucm.fdi.iw.model;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 
 /**
  * A message that users can send each other.
@@ -17,21 +24,100 @@ import org.apache.logging.log4j.Logger;
  * @author mfreire
  */
 @Entity
+@NamedQueries({
+	@NamedQuery(name="Message.countUnread",
+	query="SELECT COUNT(m) FROM Message m "
+			+ "WHERE m.recipient.id = :userId AND m.dateRead = null")
+})
 public class Message {
 	
-	@SuppressWarnings("unused")
 	private static Logger log = LogManager.getLogger(Message.class);	
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	@ManyToOne(targetEntity = User.class)
 	private User sender;
+	@ManyToOne(targetEntity = User.class)
 	private User recipient;
 	private String text;
 	
 	private LocalDateTime dateSent;
 	private LocalDateTime dateRead;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	/**
+	 * Convierte colecciones de mensajes a formato JSONificable
+	 * @param messages
+	 * @return
+	 * @throws JsonProcessingException
+	 */
+	public static List<Transfer> asTransferObjects(Collection<Message> messages) {
+		ArrayList<Transfer> all = new ArrayList<>();
+		for (Message m : messages) {
+			all.add(new Transfer(m));
+		}
+		return all;
+	}
+	
+	/**
+	 * Objeto para persistir a/de JSON
+	 * @author mfreire
+	 */
+	
+	public static class Transfer {
+		private String from;
+		private String to;
+		private String sent;
+		private String received;
+		private String text;
+		long id;
+		public Transfer(Message m) {
+			this.from = m.getSender().getUsername();
+			this.to = m.getRecipient().getUsername();
+			this.sent = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(m.getDateSent());
+			this.received = m.getDateRead() == null ?
+					null : DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(m.getDateRead());
+			this.text = m.getText();
+			this.id = m.getId();
+		}
+		public String getFrom() {
+			return from;
+		}
+		public void setFrom(String from) {
+			this.from = from;
+		}
+		public String getTo() {
+			return to;
+		}
+		public void setTo(String to) {
+			this.to = to;
+		}
+		public String getSent() {
+			return sent;
+		}
+		public void setSent(String sent) {
+			this.sent = sent;
+		}
+		public String getReceived() {
+			return received;
+		}
+		public void setReceived(String received) {
+			this.received = received;
+		}
+		public String getText() {
+			return text;
+		}
+		public void setText(String text) {
+			this.text = text;
+		}
+		public long getId() {
+			return id;
+		}
+		public void setId(long id) {
+			this.id = id;
+		}		
+	}
+	
 	public long getId() {
 		return id;
 	}
@@ -40,7 +126,6 @@ public class Message {
 		this.id = id;
 	}
 
-	@ManyToOne(targetEntity = User.class)
 	public User getSender() {
 		return sender;
 	}
@@ -49,7 +134,6 @@ public class Message {
 		this.sender = sender;
 	}
 
-	@ManyToOne(targetEntity = User.class)
 	public User getRecipient() {
 		return recipient;
 	}
