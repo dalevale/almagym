a635d0e1e8468cc0525be23529ae6b0edd559d27
isaugar <62020260+isaugar@users.noreply.github.com>
2020-06-12 22:58:07 +0200

    Add files via upload

--- src/main/java/es/ucm/fdi/iw/control/LessonController.java
+++ src/main/java/es/ucm/fdi/iw/control/LessonController.java
@@ -20,8 +20,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import es.ucm.fdi.iw.model.Inscription;
 import es.ucm.fdi.iw.model.Lesson;
 import es.ucm.fdi.iw.model.Room;
 import es.ucm.fdi.iw.model.User;
@@ -72,7 +74,6 @@ public class LessonController {
         lesson.setProfe(entityManager.find(User.class, lessonRequest.profeId)); 
         lesson.setRoom(entityManager.find(Room.class, lessonRequest.roomId)); 
         lesson.setName(lessonRequest.name);
-        lesson.setRoom(entityManager.find(Room.class, lessonRequest.roomId));
         lesson.setTotalStudents(lessonRequest.totalStudents);
         lesson.setDateIni(LocalDateTime.parse(lessonRequest.dateIni, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
         lesson.setDateFin(LocalDateTime.parse(lessonRequest.dateFin, DateTimeFormatter.ISO_LOCAL_DATE_TIME));
@@ -107,6 +108,55 @@ public class LessonController {
    		entityManager.remove(target);
    		return "exito";
     }
+    
+
+    @PostMapping("switchInscription")    
+    @ResponseBody
+    @Transactional
+    public String switchInscription(@RequestBody Inscription.Transfer inscriptionRequest) {
+    	
+//    	Inscription inscription = new Inscription();
+//    	inscription.setLesson(entityManager.find(Lesson.class, inscriptionRequest.lesson));
+//    	inscription.setUser(entityManager.find(User.class, inscriptionRequest.user));
+//    	
+//        Lesson lesson = inscription.getLesson();
+//        User usuario = inscription.getUser();
+//        
+//        if(lesson.isInscript(usuario)) {
+//        	lesson.removeInscription(usuario);
+//        }else {
+//        	lesson.addInscription(usuario);
+//        }
+//   	
+//   		
+//   		log.info("Successfully switch inscription: ", lesson.getId());
+   		return "exito";
+    }
+    
+    
+    @GetMapping(value = "switchInscription2")    
+    @Transactional
+    public String switchInscription2(@RequestParam long idUser, @RequestParam long idLesson, HttpSession session) {
+    	User usuario = entityManager.find(User.class, idUser);
+    	Lesson lesson = entityManager.find(Lesson.class, idLesson);
+        
+		if (lesson.isInscript(usuario)) {
+			lesson.removeInscription(usuario);
+		} else if(lesson.estaVacio()){
+			Inscription insc = new Inscription(usuario, lesson);
+			
+			entityManager.persist(insc);
+			lesson.getInscriptions().add(insc);
+			
+		}
+   		
+		entityManager.flush();
+		
+		
+   		log.info("Successfully switch inscription: ", lesson.getId());
+    	return "redirect:/clases/";
+    }
+    
 }
 
 // file edit selection view go run [terminal] help
